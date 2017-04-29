45f734334c67772019d921d188e7a78ddba75fe1
Sam Ezeh <spammy23@users.noreply.github.com>
2017-04-29 10:23:26 +0100

    Removed some imports

--- src/main/java/me/ezeh/spammy23/PVSListener.java
+++ src/main/java/me/ezeh/spammy23/PVSListener.java
@@ -1,13 +1,13 @@
 package me.ezeh.spammy23;
 
 import com.comphenix.protocol.ProtocolLibrary;
-import net.minecraft.server.v1_11_R1.PacketLoginOutSuccess;
 import org.bukkit.World;
-import org.bukkit.craftbukkit.v1_11_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.*;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
@@ -15,35 +15,40 @@ import java.util.UUID;
 
 
 public class PVSListener implements Listener {
+    static HashMap<UUID, Integer> versions = new HashMap<>();
     JavaPlugin plugin;
-    PVSListener(JavaPlugin pl){
+
+    PVSListener(JavaPlugin pl) {
         plugin = pl;
     }
-    static HashMap<UUID, Integer> versions = new HashMap<>();
+
     @EventHandler
-    public void onJoin(PlayerJoinEvent e){
+    public void onJoin(PlayerJoinEvent e) {
         Player p = e.getPlayer();
         Integer v = ProtocolLibrary.getProtocolManager().getProtocolVersion(p);
         versions.put(p.getUniqueId(), v);
         p.setResourcePack(getResourcePack(p.getWorld(), v));
     }
+
     @EventHandler
-    public void onPlayerLeave(PlayerQuitEvent e){
+    public void onPlayerLeave(PlayerQuitEvent e) {
         versions.remove(e.getPlayer().getUniqueId());
     }
+
     @EventHandler
-    public void onPlayerEnterWorld(PlayerTeleportEvent e){
-        if(e.getFrom().getWorld()==e.getFrom().getWorld())return;
+    public void onPlayerEnterWorld(PlayerTeleportEvent e) {
+        if (e.getFrom().getWorld() == e.getFrom().getWorld()) return;
         Player p = e.getPlayer();
         p.setResourcePack(getResourcePack(e.getTo().getWorld(), versions.get(p.getUniqueId())));
     }
-    private String getResourcePack(World w, int version){
-        String config = plugin.getConfig().getString("resources."+w.getName()+"."+version);
-        if (config==null)
-            config = plugin.getConfig().getString("resources."+w.getName()+"default");
-        if(config==null)
-            config = plugin.getConfig().getString("defaults."+version);
-        if (config==null)
+
+    private String getResourcePack(World w, int version) {
+        String config = plugin.getConfig().getString("resources." + w.getName() + "." + version);
+        if (config == null)
+            config = plugin.getConfig().getString("resources." + w.getName() + "default");
+        if (config == null)
+            config = plugin.getConfig().getString("defaults." + version);
+        if (config == null)
             config = plugin.getConfig().getString("default");
         return config;
     }
